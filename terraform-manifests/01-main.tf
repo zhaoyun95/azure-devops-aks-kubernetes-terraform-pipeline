f3f742a141abd0d1eb4ac5eab013ae3cb54b606b
James Ye <998065+zhaoyun95@users.noreply.github.com>
2021-08-11 23:07:32 -0700

    modified storage account

--- terraform-manifests/01-main.tf
+++ terraform-manifests/01-main.tf
@@ -29,7 +29,7 @@ terraform {
 # Terraform State Storage to Azure Storage Container
   backend "azurerm" {
     #resource_group_name   = "terraform-storage-rg"
-    #storage_account_name  = "terraformstatexlrwdrzs"
+    #storage_account_name  = "terraformstate123jy"
     #container_name        = "tfstatefiles"
     #key                   = "terraform-custom-vnet.tfstate"
   }  
